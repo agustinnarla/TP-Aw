5a3e7b24465f53d94eef9161faff337f5dbc5444
root <mgiraudo@ies21.edu.ar>
2023-10-31 10:58:22 -0300

    Js

---
+++ peliculas.js
@@ -0,0 +1,76 @@
+// const objetousuarios = {
+//     perfiles: [
+//         {
+//             perfil: 1,
+//             nombre: "Agus"
+//         },
+//         {
+//             perfil: 2,
+//             nombre: "Valen"
+//         },
+//         {
+//             perfil: 3,
+//             nombre: "Roberto"
+
+//         },
+//         {
+//             perfil: 4,
+//             nombre: "Maluma"
+//         },
+//         {
+//             perfil: 5,
+//             nombre: "Francis"
+//         }
+//     ]
+// }
+
+// const objPeliculas = {
+//     peliculas: [
+//         {
+//             nombre: "Harry potter ",
+//             descripcion: "Harry potter blablalbal",
+//              genero: "Fantasia, Magia"
+//              img: "./asdhkjashdkasd"
+//              link: "https://"
+//         },
+
+//         {
+//             nombre: "Da vinci ",
+//             descripcion: "Mi amigo david blablalbal",
+//             genero: "Suspenso, Historia"
+//         },
+
+//         {
+//             nombre: "Harry potter ",
+//             descripcion: "Harry potter blablalbal",
+//             genero: "Fantasia, Magia"
+//         },
+
+//         {
+//             nombre: "Harry potter ",
+//             descripcion: "Harry potter blablalbal",
+//             genero: "Fantasia, Magia"
+//         },
+
+//         {
+//             nombre: "Harry potter ",
+//             descripcion: "Harry potter blablalbal",
+//             genero: "Fantasia, Magia"
+//         }
+
+//     ]
+
+// }
+
+const catalogo = {
+    nombre: "messi",
+    balones: 8,
+    articulos:[
+        {
+            nombre: "Robert",
+            precio: "20 pe"
+        }
+    ]
+}
+
+export default catalogo
---
+++ principal.js
@@ -0,0 +1,4 @@
+import catalogo from './peliculas.js'
+console.log(catalogo)
+
+const app = document.getElementById("hola")
